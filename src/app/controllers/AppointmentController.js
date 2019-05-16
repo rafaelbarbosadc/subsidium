a4db0f60cb277cee4a680e5bd570e543457d46c1
iesalobato <iesalobato10@gmail.com>
2019-05-15 23:18:29 -0400

    add logica de reserva(sem duracao)

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,8 +2,7 @@ const { User, Appointment } = require('../models')
 
 class AppointmentController {
   async create (req, res) {
-    const provider = await User.findByPk(req.session.user.id)
-
+    const provider = await User.findByPk(req.params.provider)
     return res.render('appointments/create', { provider })
   }
 
@@ -12,13 +11,47 @@ class AppointmentController {
     const { provider } = req.params
     const { date, duration, people_quantity } = req.body
 
-    await Appointment.create({
-      user_id: id,
-      provider_id: provider,
-      date,
-      duration,
-      people_quantity
+    const count_appointment = await Appointment.count({
+      where:{
+        provider_id: provider,
+        date: date
+      }
     })
+
+    if(count_appointment == 0){
+      await Appointment.create({
+        user_id: id,
+        provider_id: provider,
+        date,
+        duration,
+        people_quantity:people_quantity
+      })
+    }else{
+
+      const people_reserved = await Appointment.sum('people_quantity',{
+        where:{
+          provider_id: provider,
+          date: date
+        }
+      })
+
+      const restaurant = await User.findByPk(provider)
+      const total_people= restaurant.people_quantity
+      const table_quantity= restaurant.table_quantity
+      const table_available = Math.trunc(table_quantity - (people_reserved/4));
+      const table_requested = Math.ceil(people_quantity/4);
+
+      if((people_reserved < total_people) && (table_requested < table_available)){
+
+        await Appointment.create({
+          user_id: id,
+          provider_id: provider,
+          date,
+          duration,
+          people_quantity:people_quantity
+        })
+      }
+    }
 
     return res.redirect('/app/dashboard')
   }
