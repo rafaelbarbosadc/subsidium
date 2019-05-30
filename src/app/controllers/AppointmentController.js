14cc4bdedda7ebd3dd2ec9d228c93c027fa12fa5
iesalobato <iesalobato10@gmail.com>
2019-05-30 19:50:24 -0400

    add logica agendamento(com duracao)

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,6 @@
 const { User, Appointment } = require('../models')
+const moment = require('moment')
+const Op = require('sequelize')
 
 class AppointmentController {
   async create (req, res) {
@@ -11,31 +13,46 @@ class AppointmentController {
     const { provider } = req.params
     const { date, duration, people_quantity } = req.body
 
+    
+    const duration_int = parseInt(duration[1])
+    const date_end = moment(date).add(duration_int,'hours')
+    
+
     const count_appointment = await Appointment.count({
       where: {
         provider_id: provider,
-        date: date
+        date: {[Op.between]:[date,date_end]}
       }
     })
+
     const restaurant = await User.findByPk(provider)
     const total_people = restaurant.people_quantity
+
     
-    if((count_appointment == 0 ) && (total_people >= people_quantity)){
-      await Appointment.create({
+  if (total_people >= people_quantity){ 
+    if(count_appointment == 0 ){
+    
+    
+    await Appointment.create({
+        user_id: id,
+        provider_id: provider,
+        date,
+        duration,
         people_quantity: people_quantity
-      })
-    } else {
+    })
+  } else {
       const people_reserved = await Appointment.sum('people_quantity', {
         where: {
           provider_id: provider,
-          date: date
+          date: {[Op.between]:[date,date_end]}
         }
       })
+      
     
       const table_quantity= restaurant.table_quantity
       const table_available = Math.trunc(table_quantity - (people_reserved/4));
       const table_requested = Math.ceil(people_quantity/4);
-      
+    
       if((people_reserved < total_people) && (table_requested < table_available)){
         
         await Appointment.create({
@@ -45,9 +62,13 @@ class AppointmentController {
           duration,
           people_quantity: people_quantity
         })
+      }else{
+        //req.flash('error','Sem vagas suficientes')
       }
     }
-
+  }else{
+   // req.flash('error','Sem vagas sufucientes')
+  }
     return res.redirect('/app/dashboard')
   }
 }
