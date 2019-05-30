edaae9aaef61856637ea38f9a2e95d7fb93ce797
Rafael Barbosa <rafaelbarbosadc@gmail.com>
2019-05-30 15:48:24 -0400

    Varias mudanças que nem lembro mais

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -12,43 +12,41 @@ class AppointmentController {
     const { date, duration, people_quantity } = req.body
 
     const count_appointment = await Appointment.count({
-      where:{
+      where: {
         provider_id: provider,
         date: date
       }
     })
 
-    if(count_appointment == 0){
+    if (count_appointment == 0) {
       await Appointment.create({
         user_id: id,
         provider_id: provider,
         date,
         duration,
-        people_quantity:people_quantity
+        people_quantity: people_quantity
       })
-    }else{
-
-      const people_reserved = await Appointment.sum('people_quantity',{
-        where:{
+    } else {
+      const people_reserved = await Appointment.sum('people_quantity', {
+        where: {
           provider_id: provider,
           date: date
         }
       })
 
       const restaurant = await User.findByPk(provider)
-      const total_people= restaurant.people_quantity
-      const table_quantity= restaurant.table_quantity
-      const table_available = Math.trunc(table_quantity - (people_reserved/4));
-      const table_requested = Math.ceil(people_quantity/4);
+      const total_people = restaurant.people_quantity
+      const table_quantity = restaurant.table_quantity
+      const table_available = Math.trunc(table_quantity - people_reserved / 4)
+      const table_requested = Math.ceil(people_quantity / 4)
 
-      if((people_reserved < total_people) && (table_requested < table_available)){
-
+      if (people_reserved < total_people && table_requested < table_available) {
         await Appointment.create({
           user_id: id,
           provider_id: provider,
           date,
           duration,
-          people_quantity:people_quantity
+          people_quantity: people_quantity
         })
       }
     }
--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,10 +1,51 @@
-const { User } = require('../models')
-
+const { User, Appointment } = require('../models')
+const { Op } = require('sequelize')
+const moment = require('moment')
 class DashboardController {
   async index (req, res) {
     const providers = await User.findAll({ where: { provider: true } })
+    const peopleQuantity = await Appointment.sum('people_quantity', {
+      where: {
+        provider_id: req.session.user.id,
+        date: {
+          [Op.between]: [
+            moment()
+              .startOf('day')
+              .format(),
+            moment()
+              .endOf('day')
+              .format()
+          ]
+        }
+      }
+    })
 
-    return res.render('dashboard', { providers })
+    const reserveQuantity = await Appointment.count({
+      distinct: true,
+      col: 'Appointment.id',
+      where: {
+        provider_id: req.session.user.id,
+        date: {
+          [Op.between]: [
+            moment()
+              .startOf('day')
+              .format(),
+            moment()
+              .endOf('day')
+              .format()
+          ]
+        }
+      },
+      order: [['date', 'ASC']]
+    })
+
+    console.log(reserveQuantity)
+
+    return res.render('dashboard', {
+      providers,
+      peopleQuantity,
+      reserveQuantity
+    })
   }
 
   async editProfile (req, res) {
