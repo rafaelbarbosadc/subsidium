5aa695d24957f721030ce5e57c2511ea78ad296d
Rafael Barbosa <rafaelbarbosadc@gmail.com>
2019-05-30 23:16:01 -0400

    Correçao de erros

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,7 +1,7 @@
 const { User, Appointment } = require('../models')
 const moment = require('moment')
 const Op = require('sequelize')
-
+const nodemailer = require('nodemailer')
 class AppointmentController {
   async create (req, res) {
     const provider = await User.findByPk(req.params.provider)
@@ -34,56 +34,53 @@ class AppointmentController {
           duration,
           people_quantity: people_quantity
         })
-      } else {
-        const people_reserved = await Appointment.sum('people_quantity', {
-          where: {
-            provider_id: provider,
-            date: { [Op.between]: [date, date_end] }
-          }
-        })
-
-        const table_quantity = restaurant.table_quantity
-        const table_available = Math.trunc(table_quantity - people_reserved / 4)
-        const table_requested = Math.ceil(people_quantity / 4)
-
-        if (
-          people_reserved < total_people &&
-          table_requested < table_available
-        ) {
-          await Appointment.create({
-            user_id: id,
-            provider_id: provider,
-            date,
-            duration,
-            people_quantity: people_quantity
-          })
-        } else {
-          // req.flash('error','Sem vagas suficientes')
-        }
       }
-
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: EMAIL,
-          pass: PASSWORD
+    } else {
+      const people_reserved = await Appointment.sum('people_quantity', {
+        where: {
+          provider_id: provider,
+          date: { [Op.between]: [date, date_end] }
         }
       })
 
-      const mailOptions = {
-        from: EMAIL,
-        to: [user.email, provider.email].join(),
-        subject: 'Confirmação de Agendamento',
-        html:
-          '<p>Olá<br />Só passamos para avisá-lo de que seu agendamento foi efetuado com sucesso!<br />Este é um email automático, favor não respondê-lo.</p>'
+      const table_quantity = restaurant.table_quantity
+      const table_available = Math.trunc(table_quantity - people_reserved / 4)
+      const table_requested = Math.ceil(people_quantity / 4)
+
+      if (people_reserved < total_people && table_requested < table_available) {
+        await Appointment.create({
+          user_id: id,
+          provider_id: provider,
+          date,
+          duration,
+          people_quantity: people_quantity
+        })
+      } else {
+        // req.flash('error','Sem vagas suficientes')
       }
-
-      let info = await transporter.sendMail(mailOptions)
-
-      console.log('Mensagem enviada: %s', info.messageId)
-
-      return res.redirect('/app/dashboard')
     }
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: EMAIL,
+        pass: PASSWORD
+      }
+    })
+
+    const mailOptions = {
+      from: EMAIL,
+      to: [req.session.user.email, restaurant.email].join(),
+      subject: 'Confirmação de Agendamento',
+      html:
+        '<p>Olá<br />Só passamos para avisá-lo de que seu agendamento foi efetuado com sucesso!<br />Este é um email automático, favor não respondê-lo.</p>'
+    }
+
+    let info = await transporter.sendMail(mailOptions)
+
+    console.log('Mensagem enviada: %s', info.messageId)
+
+    return res.redirect('/app/dashboard')
   }
 }
 
